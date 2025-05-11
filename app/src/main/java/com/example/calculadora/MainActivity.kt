ac25b76d6817bb6512f9db56660e4d5b8ded8ed7
PedroLucasCG <ContatoPedroLucasCG@gmail.com>
2025-05-10 22:35:34 -0300

    calculadora completa - revisões pendentes

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -12,8 +12,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import net.objecthunter.exp4j.ExpressionBuilder
 import java.math.BigDecimal
-import kotlin.math.pow
-import kotlin.math.sqrt
+import kotlin.math.*
 
 @Suppress("NAME_SHADOWING")
 class MainActivity : AppCompatActivity() {
@@ -34,8 +33,10 @@ class MainActivity : AppCompatActivity() {
         var isExpOperation = false
         var isMod = false
         var lastEntrySize = 0;
+        var memory: Double = 0.0
         var equation = ""
         val e = 2.7182818284590452353602874713527
+        val pi = 3.1415926535897932384626433832795
         val display = findViewById<TextView>(R.id.textView)
         val displaySmall = findViewById<TextView>(R.id.textViewSmall)
         val digitButtons = listOf(
@@ -80,6 +81,17 @@ class MainActivity : AppCompatActivity() {
             "moduleOfX" to R.id.moduleOfX,
             "exp" to R.id.exp,
             "mod" to R.id.mod,
+            "ex" to R.id.ex,
+            "pi" to R.id.pi,
+            "e" to R.id.e,
+            "c" to R.id.c,
+            "backspace" to R.id.backspace,
+            "MS" to R.id.MS,
+            "MR" to R.id.MR,
+            "MC" to R.id.MC,
+            "M-" to R.id.Mminus,
+            "M+" to R.id.Mplus,
+            "DEG" to R.id.deg,
         )
 
         val actionButtons: Map<String, Button> = actionButtonIds
@@ -95,6 +107,7 @@ class MainActivity : AppCompatActivity() {
                 equation += display.text.toString()
             }
             equation = equation.replace(oldValue = "e", newValue = e.toString())
+            equation = equation.replace(oldValue = "π", newValue = pi.toString())
             val result = eval(equation)
             val roundedString = String.format("%.2f", result)
             val rounded = roundedString.toDouble()
@@ -121,10 +134,11 @@ class MainActivity : AppCompatActivity() {
                 total *= c
             }
             equation = equation.dropLast(display.text.length)
-            displaySmall.text = displaySmall.text.dropLast(lastEntrySize)
+            displaySmall.text = displaySmall.text.dropLast(display.text.length)
             displaySmall.text = displaySmall.text.toString() + total.toString()
             equation += total.toString()
             display.text = "0"
+            readOnlyDisplay = true
         }
 
         actionButtons["sqrt2"]?.setOnClickListener {
@@ -136,7 +150,6 @@ class MainActivity : AppCompatActivity() {
             equation += "^"
             displaySmall.text = equation
             display.text = "0"
-            readOnlyDisplay = true
         }
 
         actionButtons["tenPowerX"]?.setOnClickListener {
@@ -216,6 +229,76 @@ class MainActivity : AppCompatActivity() {
             isMod = true
         }
 
+        actionButtons["ex"]?.setOnClickListener {
+            val result = e.pow(display.text.toString().toDouble()).toString()
+            equation += result.toString()
+            display.text = result.toString()
+            displaySmall.text = equation
+            readOnlyDisplay = true
+        }
+
+        actionButtons["pi"]?.setOnClickListener {
+            equation += "π"
+            display.text = "π"
+            displaySmall.text = equation
+            readOnlyDisplay = true
+        }
+
+        actionButtons["e"]?.setOnClickListener {
+            equation += "e"
+            display.text = "e"
+            displaySmall.text = equation
+            readOnlyDisplay = true
+        }
+
+        actionButtons["c"]?.setOnClickListener {
+            if (actionButtons["c"]?.text == "CE") {
+                display.text = "0"
+            } else {
+                displaySmall.text = "0"
+                equation = ""
+            }
+
+            if (display.text.toString() != "0") {
+                actionButtons["c"]?.text = "CE"
+            } else {
+                actionButtons["c"]?.text = "C"
+            }
+        }
+
+        actionButtons["backspace"]?.setOnClickListener {
+            display.text = display.text.dropLast(1)
+            if (display.text.isEmpty()) {
+                display.text = "0"
+            }
+        }
+
+        actionButtons["MS"]?.setOnClickListener {
+            memory = display.text.toString().toDouble()
+        }
+
+        actionButtons["MR"]?.setOnClickListener {
+            if (memory != 0.0) {
+                display.text = memory.toString()
+            }
+        }
+
+        actionButtons["M+"]?.setOnClickListener {
+            memory += display.text.toString().toDouble()
+        }
+
+        actionButtons["M-"]?.setOnClickListener {
+            memory -= display.text.toString().toDouble()
+        }
+
+        actionButtons["DEG"]?.setOnClickListener {
+            if (actionButtons["DEG"]?.text == "DEG") {
+                actionButtons["DEG"]?.text = "GRAD"
+            }else {
+                actionButtons["DEG"]?.text = "DEG"
+            }
+        }
+
         operationButtons.forEach  { btn ->
             btn.setOnClickListener {
                 lastEntrySize = display.text.length
@@ -229,59 +312,70 @@ class MainActivity : AppCompatActivity() {
 
         digitButtons.forEach { btn ->
             btn.setOnClickListener {
-                if (isExpOperation) {
-                    equation = equation.dropLast(2)
-                    equation += btn.text.toString() + ")"
-                    displaySmall.text = equation
-                    return@setOnClickListener
-                }
-
-                if (isMod) {
-                    equation += btn.text.toString() + ")"
-                    displaySmall.text = equation
-                    isMod = false
-                    return@setOnClickListener
-                }
-
-                if (btn.text.toString() == ")" || btn.text.toString() == "(") {
-                    val openCount  = equation.count { it == '(' }
-                    val closeCount = equation.count { it == ')' }
-
-                    if (display.text.toString() == "0") {
-                        equation += "("
+                try {
+                    if (isExpOperation) {
+                        equation = equation.dropLast(2)
+                        equation += btn.text.toString() + ")"
                         displaySmall.text = equation
                         return@setOnClickListener
                     }
-                    if (openCount == closeCount && btn.text.toString() == ")") {
+
+                    if (isMod) {
+                        equation += btn.text.toString() + ")"
+                        displaySmall.text = equation
+                        isMod = false
                         return@setOnClickListener
-                    } else {
+                    }
+
+                    if (btn.text.toString() == ")" || btn.text.toString() == "(") {
+                        val openCount  = equation.count { it == '(' }
+                        val closeCount = equation.count { it == ')' }
+
+                        if (display.text.toString() == "0") {
+                            equation += "("
+                            displaySmall.text = equation
+                            return@setOnClickListener
+                        }
+                        if (openCount == closeCount && btn.text.toString() == ")") {
+                            return@setOnClickListener
+                        } else {
+                            equation += btn.text.toString()
+                            displaySmall.text = equation
+                            return@setOnClickListener
+                        }
+                    }
+
+                    if (readOnlyDisplay) {
+                        equation = btn.text.toString()
+                        display.text = equation
+                        displaySmall.text = ""
+                        readOnlyDisplay = false
+                        return@setOnClickListener
+                    }
+                    if (display.text.toString() == "0" && btn.text.toString() != ".") {
+                        display.text = btn.text
                         equation += btn.text.toString()
-                        displaySmall.text = equation
                         return@setOnClickListener
                     }
-                }
 
-                if (readOnlyDisplay) {
-                    equation = btn.text.toString()
-                    display.text = equation
-                    displaySmall.text = ""
-                    readOnlyDisplay = false
-                    return@setOnClickListener
-                }
-                if (display.text.toString() == "0" && btn.text.toString() != ".") {
-                    display.text = btn.text
+                    if (btn.text.toString() == ".") {
+                        if (display.text.toString().contains('.')) {
+                            return@setOnClickListener
+                        }
+                    }
+
                     equation += btn.text.toString()
-                    return@setOnClickListener
-                }
-
-                if (btn.text.toString() == ".") {
-                    if (display.text.toString().contains('.')) {
-                        return@setOnClickListener
+                    display.text = display.text.toString() + btn.text.toString()
+                } catch (e: Exception) {
+                    readOnlyDisplay = true
+                } finally {
+                    if (display.text.toString() != "0") {
+                        actionButtons["c"]?.text = "CE"
+                    } else {
+                        actionButtons["c"]?.text = "C"
                     }
                 }
 
-                equation += btn.text.toString()
-                display.text = display.text.toString() + btn.text.toString()
             }
         }
 
@@ -290,12 +384,39 @@ class MainActivity : AppCompatActivity() {
             PopupMenu(this, view).apply {
                 menuInflater.inflate(R.menu.menu_trig, menu)
                 setOnMenuItemClickListener { item ->
-                    when(item.itemId) {
-                        R.id.deg  -> println()
-                        R.id.rad  -> println()
-                        R.id.grad -> println()
+                    var input = display.text.toString().toDoubleOrNull()
+                        ?: return@setOnMenuItemClickListener false
+
+                    if (actionButtons["DEG"]?.text.toString() == "DEG") {
+                        input = Math.toRadians(input)
                     }
-                    true
+
+                    val result: Double? = when (item.itemId) {
+                        R.id.sin  -> sin(input)
+                        R.id.cos  -> cos(input)
+                        R.id.tan  -> tan(input)
+                        R.id.sec  -> 1.0 / cos(input)
+                        R.id.csc  -> 1.0 / sin(input)
+                        R.id.cot  -> 1.0 / tan(input)
+
+                        R.id.asin -> asin(input)
+                        R.id.acos -> acos(input)
+                        R.id.atan -> atan(input)
+                        R.id.asec -> acos(1.0 / input)
+                        R.id.acsc -> asin(1.0 / input)
+                        R.id.acot -> atan(1.0 / input)
+
+                        else      -> null
+                    }
+
+                    return@setOnMenuItemClickListener result?.let { value ->
+                        val text = value.toString()
+                        equation = equation.dropLast(display.text.length) + text
+                        display.text = text
+                        displaySmall.text = equation
+                        readOnlyDisplay = true
+                        true
+                    } ?: false
                 }
             }.show()
         }
@@ -305,16 +426,35 @@ class MainActivity : AppCompatActivity() {
             PopupMenu(this, view).apply {
                 menuInflater.inflate(R.menu.menu_function, menu)
                 setOnMenuItemClickListener { item ->
-                    when(item.itemId) {
-                        R.id.module  -> println()
-                        R.id.floor  -> println()
-                        R.id.ceiling -> println()
+                    var input = display.text.toString().toDoubleOrNull()
+                        ?: return@setOnMenuItemClickListener false
 
-                        R.id.rand  -> println()
-                        R.id.dms  -> println()
-                        R.id.deg -> println()
+                    val result: Double? = when(item.itemId) {
+                        R.id.module  -> abs(input)
+                        R.id.floor  -> floor(input)
+                        R.id.ceiling -> ceil(input)
+
+                        R.id.rand  -> Math.random()
+                        R.id.dms   -> {
+                            val totalDeg = input
+                            val d = totalDeg.toInt()
+                            val mTotal = (totalDeg - d) * 60.0
+                            val m = mTotal.toInt()
+                            val s = ((mTotal - m) * 60.0)
+
+                            d + m / 100.0 + s / 10000.0
+                        }
+                        R.id.deg   -> Math.toDegrees(input)
+                        else       -> null
                     }
-                    true
+                    return@setOnMenuItemClickListener result?.let { value ->
+                        val text = value.toString()
+                        equation = equation.dropLast(display.text.length) + text
+                        display.text = text
+                        displaySmall.text = equation
+                        readOnlyDisplay = true
+                        true
+                    } ?: false
                 }
             }.show()
         }
